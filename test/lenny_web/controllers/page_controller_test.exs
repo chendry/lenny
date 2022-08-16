bb4ef3408f5e9cc7e4a13a6a5286c1dca65a5d59
Chad Hendry <chendry@chendry.org>
2022-08-16 05:12:39 -0500

    gutting generated html/css

--- test/lenny_web/controllers/page_controller_test.exs
+++ test/lenny_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LennyWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Lenny"
   end
 end
