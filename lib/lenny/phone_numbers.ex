ea85d23cbb35fd578261c6fe03fb91aff6269865
Chad Hendry <chendry@chendry.org>
2022-08-17 06:53:11 -0500

    registering phone numbers and starting twilio verification

--- lib/lenny/phone_numbers.ex
+++ lib/lenny/phone_numbers.ex
@@ -1,11 +1,59 @@
 defmodule Lenny.PhoneNumbers do
+  import Ecto.Query
+  import Ecto.Changeset
+
   alias Lenny.Repo
   alias Lenny.Accounts.User
   alias Lenny.PhoneNumbers.PhoneNumber
+  alias Lenny.Twilio
+
+  def get_approved_phone_number(%User{} = user) do
+    PhoneNumber
+    |> for_user(user)
+    |> where([p], p.status == "approved")
+    |> limit(1)
+    |> Repo.one()
+  end
+
+  def get_pending_phone_number(%User{} = user) do
+    PhoneNumber
+    |> for_user(user)
+    |> where([p], is_nil(p.status) or p.status == "pending")
+    |> limit(1)
+    |> Repo.one()
+  end
+
+  defp for_user(query, %User{} = user) do
+    query
+    |> where([p], p.user_id == ^user.id)
+    |> where([p], is_nil(p.deleted_at))
+    |> order_by([p], desc: p.id)
+  end
 
   def register_phone_number(%User{} = user, phone_number) do
     %PhoneNumber{user_id: user.id}
     |> PhoneNumber.changeset(%{phone: phone_number})
     |> Repo.insert()
+  end
+
+  def start_new_verification(%PhoneNumber{} = phone_number) do
+    case Twilio.start_new_verification(phone_number.phone, "sms") do
+      {:ok, sid} ->
+        phone_number =
+          phone_number
+          |> change(sid: sid)
+          |> Repo.update!()
+
+        {:ok, phone_number}
+
+      :invalid_phone_number ->
+        changeset =
+          phone_number
+          |> PhoneNumber.changeset(%{})
+          |> Map.put(:action, :insert)
+          |> Ecto.Changeset.add_error(:phone, "is invalid according to twilio")
+
+        {:error, changeset}
+    end
   end
 end
--- lib/lenny_web/live/lenny_live.ex
+++ lib/lenny_web/live/lenny_live.ex
@@ -2,19 +2,59 @@ defmodule LennyWeb.LennyLive do
   use LennyWeb, :live_view
 
   alias Lenny.Accounts
+  alias Lenny.PhoneNumbers
+  alias Lenny.PhoneNumbers.PhoneNumber
+  alias Lenny.Twilio
 
   @impl true
   def mount(_params, %{"user_token" => user_token}, socket) do
+    user = Accounts.get_user_by_session_token(user_token)
+
     {:ok,
      socket
-     |> assign(:current_user, Accounts.get_user_by_session_token(user_token))}
+     |> assign(:user, user)
+     |> assign(:changeset, PhoneNumber.changeset(%PhoneNumber{}, %{}))
+     |> assign(:pending_phone_number, PhoneNumbers.get_pending_phone_number(user))
+     |> assign(:approved_phone_number, PhoneNumbers.get_approved_phone_number(user))}
   end
 
   @impl true
   def render(assigns) do
     ~H"""
     <h1>Lenny</h1>
-    <p><%= @current_user.email %></p>
+    <p><%= @user.email %></p>
+
+    <%= if @pending_phone_number == nil and @approved_phone_number == nil do %>
+      <.form for={@changeset} let={f} phx-submit="register_phone_number">
+        <%= telephone_input f, :phone %>
+        <%= error_tag f, :phone %>
+        <%= submit "Submit" %>
+      </.form>
+    <% end %>
+
+    <%= if @pending_phone_number do %>
+    <% end %>
+
+    <p><%= inspect @pending_phone_number %></p>
+    <p><%= inspect @approved_phone_number %></p>
+    <p><%= inspect @changeset %></p>
     """
+  end
+
+  @impl true
+  def handle_event("register_phone_number", %{"phone_number" => %{"phone" => phone}}, socket) do
+    case PhoneNumbers.register_phone_number(socket.assigns.user, phone) do
+      {:error, changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
+
+      {:ok, phone_number} ->
+        case PhoneNumbers.start_new_verification(phone_number) do
+          {:ok, phone_number} ->
+            {:noreply, assign(socket, :pending_phone_number, phone_number)}
+
+          {:error, changeset} ->
+            {:noreply, assign(socket, :changeset, changeset)}
+        end
+    end
   end
 end
