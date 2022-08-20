2c2acefdbbd2bb55eabd15c43e2770f99a761aa7
Chad Hendry <chendry@chendry.org>
2022-08-20 10:20:14 -0500

    settling on a testing style
    Instead of passing long pipelines to assert, assign the output of the
    pipeline to a html variable and assert on that.  That also allows for
    multiple assertions on the html.  I figure it's best to just do this
    consistently; we'll see where it goes.

--- test/lenny_web/live/lenny_live_test.exs
+++ test/lenny_web/live/lenny_live_test.exs
@@ -14,26 +14,40 @@ defmodule LennyWeb.LennyLiveTest do
 
     assert html =~ ~r{<h1.*>\s*Register a phone number}
 
-    assert lenny_live
-           |> form("form", %{"phone_number[phone]" => "555-555-5555"})
-           |> render_submit() =~ "invalid format"
+    html =
+      lenny_live
+      |> form("form", %{"phone_number[phone]" => "555-555-5555"})
+      |> render_submit()
+
+    assert html =~ ~r{<h1.*>\s*Register a phone number}
+    assert html =~ "has invalid format"
 
     Lenny.TwilioMock
     |> expect(:verify_start, fn "+13126180256", "sms" -> {:ok, "VE-XXXX"} end)
     |> expect(:verify_check, fn "VE-XXXX", "1234" -> {:error, "invalid according to twilio"} end)
     |> expect(:verify_check, fn "VE-XXXX", "5678" -> :ok end)
 
-    assert lenny_live
-           |> form("form", %{"phone_number[phone]" => "+13126180256"})
-           |> render_submit() =~ ~r{<h1.*>\s*Verify your phone number}
+    html =
+      lenny_live
+      |> form("form", %{"phone_number[phone]" => "+13126180256"})
+      |> render_submit()
 
-    assert lenny_live
-           |> form("form", %{"verification_form[code]" => "1234"})
-           |> render_submit() =~ "invalid according to twilio"
+    assert html =~ ~r{<h1.*>\s*Verify your phone number}
 
-    assert lenny_live
-           |> form("form", %{"verification_form[code]" => "5678"})
-           |> render_submit() =~ "Approved: +13126180256"
+    html =
+      lenny_live
+      |> form("form", %{"verification_form[code]" => "1234"})
+      |> render_submit()
+
+    assert html =~ ~r{<h1.*>\s*Verify your phone number}
+    assert html =~ "invalid according to twilio"
+
+    html =
+      lenny_live
+      |> form("form", %{"verification_form[code]" => "5678"})
+      |> render_submit()
+
+    assert html =~ "Approved: +13126180256"
   end
 
   test "change a number", %{conn: conn, user: user} do
@@ -46,11 +60,15 @@ defmodule LennyWeb.LennyLiveTest do
 
     {:ok, lenny_live, html} = live(conn, "/lenny")
 
+    assert html =~ ~r{<h1.*>\s*Waiting for a forwarded call}
     assert html =~ "Approved: +13126180256"
 
-    assert lenny_live
-           |> element("button", "Change")
-           |> render_click() =~ ~r{<h1.*>\s*Change your phone number}
+    html =
+      lenny_live
+      |> element("button", "Change")
+      |> render_click()
+
+    assert html =~ ~r{<h1.*>\s*Change your phone number}
 
     Lenny.TwilioMock
     |> expect(:verify_start, fn "+13125551234", "sms" -> {:ok, "VE-XXXX"} end)
@@ -83,11 +101,15 @@ defmodule LennyWeb.LennyLiveTest do
 
     {:ok, lenny_live, html} = live(conn, "/lenny")
 
+    assert html =~ ~r{<h1.*>\s*Waiting for a forwarded call}
     assert html =~ "Approved: +15551112222"
 
-    assert lenny_live
-           |> element("button", "Change")
-           |> render_click() =~ ~r{<h1.*>\s*Change your phone number}
+    html =
+      lenny_live
+      |> element("button", "Change")
+      |> render_click()
+
+    assert html =~ ~r{<h1.*>\s*Change your phone number}
 
     Lenny.TwilioMock
     |> expect(:verify_start, fn "+15551113333", "sms" -> {:ok, "VE-XXXX"} end)
