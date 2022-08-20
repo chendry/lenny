0e6d6a5ebc620e6b7c2a6c09d924dd0491c8d28c
Chad Hendry <chendry@chendry.org>
2022-08-20 08:55:43 -0500

    adding test for changing a number

--- test/lenny_web/live/lenny_live_test.exs
+++ test/lenny_web/live/lenny_live_test.exs
@@ -4,6 +4,9 @@ defmodule LennyWeb.LennyLiveTest do
   import Mox
   import Phoenix.LiveViewTest
 
+  alias Lenny.Repo
+  alias Lenny.PhoneNumbers.PhoneNumber
+
   setup [:register_and_log_in_user]
 
   test "register a number", %{conn: conn} do
@@ -29,5 +32,42 @@ defmodule LennyWeb.LennyLiveTest do
     assert lenny_live
            |> form("form", %{"verification_form[code]" => "5678"})
            |> render_submit() =~ "Approved: +13126180256"
+  end
+
+  test "change a number", %{conn: conn, user: user} do
+    %PhoneNumber{
+      user_id: user.id,
+      phone: "+13126180256",
+      status: "approved"
+    }
+    |> Repo.insert!()
+
+    {:ok, lenny_live, html} = live(conn, "/lenny")
+
+    assert html =~ "Approved: +13126180256"
+
+    assert lenny_live
+           |> element("button", "Change")
+           |> render_click() =~ "Change your phone number"
+
+    Lenny.TwilioMock
+    |> expect(:verify_start, fn "+13125551234", "sms" -> {:ok, "VE-XXXX"} end)
+    |> expect(:verify_check, fn "VE-XXXX", "9999" -> :ok end)
+
+    html =
+      lenny_live
+      |> form("form", %{"phone_number[phone]" => "+13125551234"})
+      |> render_submit()
+
+    assert html =~ "Approved: +13126180256"
+    assert html =~ "Pending: +13125551234"
+
+    html =
+      lenny_live
+      |> form("form", %{"verification_form[code]" => "9999"})
+      |> render_submit()
+
+    refute html =~ "Pending: +13125551234"
+    assert html =~ "Approved: +13125551234"
   end
 end
