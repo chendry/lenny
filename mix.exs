a566c0b525eac749443904f1202a053d4e2313c9
Chad Hendry <chendry@chendry.org>
2022-08-20 07:49:31 -0500

    adding mox

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Lenny.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:httpoison, "~> 1.8"}
+      {:httpoison, "~> 1.8"},
+      {:mox, "~> 1.0"}
     ]
   end
 
