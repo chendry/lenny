d2e2da41b7c517e5ae37d9a3e4c95c13879f7625
Chad Hendry <chendry@chendry.org>
2022-08-21 19:48:53 -0500

    updating test with mostly real example data from twilio

--- test/lenny_web/controllers/twilio_controller_test.exs
+++ test/lenny_web/controllers/twilio_controller_test.exs
@@ -1,8 +1,37 @@
 defmodule LennyWeb.TwilioControllerTest do
   use LennyWeb.ConnCase
 
-  test "POST /", %{conn: conn} do
-    conn = post(conn, "/twilio", %{"CallSid" => "foobar"})
+  test "POST /twilio", %{conn: conn} do
+    params = %{
+      "AccountSid" => "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+      "ApiVersion" => "2010-04-01",
+      "CallSid" => "CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+      "CallStatus" => "ringing",
+      "CallToken" => "...",
+      "Called" => "+19384653669",
+      "CalledCity" => "",
+      "CalledCountry" => "US",
+      "CalledState" => "AL",
+      "CalledZip" => "",
+      "Caller" => "+13126180256",
+      "CallerCity" => "CHICAGO",
+      "CallerCountry" => "US",
+      "CallerState" => "IL",
+      "CallerZip" => "60605",
+      "Direction" => "inbound",
+      "From" => "+13126180256",
+      "FromCity" => "CHICAGO",
+      "FromCountry" => "US",
+      "FromState" => "IL",
+      "FromZip" => "60605",
+      "To" => "+19384653669",
+      "ToCity" => "",
+      "ToCountry" => "US",
+      "ToState" => "AL",
+      "ToZip" => ""
+    }
+
+    conn = post(conn, "/twilio", params)
     assert response(conn, 200) =~ "lenny_01.mp3"
   end
 end
