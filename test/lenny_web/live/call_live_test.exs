2c792c3da790c1d81ea39c84fbdad2c040f9c2dc
Chad Hendry <chendry@chendry.org>
2022-08-23 11:58:18 -0500

    splitting LennyLiveTest into CallLiveTest and PhoneNumberLiveTest

---
+++ test/lenny_web/live/call_live_test.exs
@@ -0,0 +1,60 @@
+defmodule LennyWeb.CallLiveTest do
+  use LennyWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  alias Lenny.Repo
+  alias Lenny.Calls.Call
+  alias Lenny.PhoneNumbers.PhoneNumber
+
+  setup [:register_and_log_in_user]
+
+  test "handle a call", %{conn: conn, user: user} do
+    %PhoneNumber{
+      user_id: user.id,
+      phone: "+13126180256",
+      verified_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
+    }
+    |> Repo.insert!()
+
+    {:ok, lenny_live, html} = live(conn, "/calls")
+    assert html =~ "Waiting for a forwarded call..."
+    refute html =~ "Active call:"
+
+    Phoenix.ConnTest.build_conn()
+    |> post("/twilio/incoming", %{"CallSid" => "CAXXX", "From" => "+13126180256"})
+
+    html = render(lenny_live)
+    refute html =~ "Waiting for a forwarded call..."
+    assert html =~ "Active call: CAXXX"
+
+    Phoenix.ConnTest.build_conn()
+    |> post("/twilio/status/call", %{"CallSid" => "CAXXX", "CallStatus" => "completed"})
+
+    html = render(lenny_live)
+    assert html =~ "Waiting for a forwarded call..."
+    refute html =~ "Active call: CAXXX"
+  end
+
+  test "load page with an active call in progress", %{conn: conn, user: user} do
+    %PhoneNumber{
+      user_id: user.id,
+      phone: "+13126180256",
+      verified_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
+    }
+    |> Repo.insert!()
+
+    %Call{
+      sid: "CAXXXX1234",
+      from: "+13126180256",
+      to: "+18384653669",
+      ended_at: nil,
+    }
+    |> Repo.insert!()
+
+    {:ok, _lenny_live, html} = live(conn, "/calls")
+
+    refute html =~ "Waiting for a forwarded call..."
+    assert html =~ "Active call: CAXXXX1234"
+  end
+end
--- test/lenny_web/live/phone_number_live_test.exs
+++ test/lenny_web/live/phone_number_live_test.exs
@@ -1,4 +1,4 @@
-defmodule LennyWeb.LennyLiveTest do
+defmodule LennyWeb.PhoneNumberLiveTest do
   use LennyWeb.ConnCase
 
   import Mox
@@ -136,55 +136,6 @@ defmodule LennyWeb.LennyLiveTest do
 
     assert html =~ "Approved: +15551112222"
     refute html =~ "Pending: +15551113333"
-  end
-
-  test "handle a call", %{conn: conn, user: user} do
-    %PhoneNumber{
-      user_id: user.id,
-      phone: "+13126180256",
-      verified_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
-    }
-    |> Repo.insert!()
-
-    {:ok, lenny_live, html} = live(conn, "/calls")
-    assert html =~ "Waiting for a forwarded call..."
-    refute html =~ "Active call:"
-
-    Phoenix.ConnTest.build_conn()
-    |> post("/twilio/incoming", %{"CallSid" => "CAXXX", "From" => "+13126180256"})
-
-    html = render(lenny_live)
-    refute html =~ "Waiting for a forwarded call..."
-    assert html =~ "Active call: CAXXX"
-
-    Phoenix.ConnTest.build_conn()
-    |> post("/twilio/status/call", %{"CallSid" => "CAXXX", "CallStatus" => "completed"})
-
-    html = render(lenny_live)
-    assert html =~ "Waiting for a forwarded call..."
-    refute html =~ "Active call: CAXXX"
-  end
-
-  test "load page with an active call in progress", %{conn: conn, user: user} do
-    %PhoneNumber{
-      user_id: user.id,
-      phone: "+13126180256",
-      verified_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
-    }
-    |> Repo.insert!()
-
-    %Call{
-      sid: "CAXXXX1234",
-      from: "+13126180256",
-      to: "+18384653669",
-      ended_at: nil,
-    }
-    |> Repo.insert!()
-
-    {:ok, _lenny_live, html} = live(conn, "/calls")
-
-    refute html =~ "Waiting for a forwarded call..."
-    assert html =~ "Active call: CAXXXX1234"
   end
 
   test "change number to a number with an active call", %{conn: conn, user: user} do
