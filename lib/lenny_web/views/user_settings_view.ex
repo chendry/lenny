d7707c64d015fdddbb0e4e8d021f3a348d8c275f
Chad Hendry <chendry@chendry.org>
2022-08-26 12:46:11 -0500

    styling the settings page

--- lib/lenny_web/views/user_settings_view.ex
+++ lib/lenny_web/views/user_settings_view.ex
@@ -1,3 +1,12 @@
 defmodule LennyWeb.UserSettingsView do
   use LennyWeb, :view
+
+  def settings_section(assigns) do
+    ~H"""
+    <div class="mt-8 bg-gray-100 border border-gray-600 rounded-lg p-6 pt-4">
+      <h3 class="text-lg font-bold mb-4"><%= @title %></h3>
+      <%= render_slot(@inner_block) %>
+    </div>
+    """
+  end
 end
