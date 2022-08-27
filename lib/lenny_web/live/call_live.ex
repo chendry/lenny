3d392654ac9945f9e39d0f4cb984cdade37de3e2
Chad Hendry <chendry@chendry.org>
2022-08-26 19:56:52 -0500

    using Calls.get_effective_number

--- lib/lenny_web/live/call_live.ex
+++ lib/lenny_web/live/call_live.ex
@@ -29,7 +29,7 @@ defmodule LennyWeb.CallLive do
       <p class="mt-2">
         Incoming call from
         <span id="call-from" class="font-bold text-green-700 tracking-widest">
-          <%= @call.forwarded_from || @call.from %>
+          <%= Calls.get_effective_number(@call) %>
         </span>
       </p>
 
