2839715b66a7a4be7b4a3dbb5267c694bab359ef
Chad Hendry <chendry@chendry.org>
2022-09-01 10:07:48 -0500

    adding settings tabs, styling settings

--- lib/lenny_web/views/user_settings_view.ex
+++ lib/lenny_web/views/user_settings_view.ex
@@ -13,20 +13,28 @@ defmodule LennyWeb.UserSettingsView do
 
   def tabs(assigns) do
     ~H"""
-    <div id="tabs" class="flex flex-row font-bold text-blue-600 space-x-2">
-      <%= link("Settings", to: Routes.user_settings_path(@conn, :edit_settings)) %>
-      <%= link("Email", to: Routes.user_settings_path(@conn, :edit_email)) %>
-      <%= link("Password", to: Routes.user_settings_path(@conn, :edit_password)) %>
+    <div id="tabs" class="flex flex-row mb-4 font-bold text-blue-600 border-b-2 border-blue-600 space-x-3">
+      <.tab conn={@conn} label="Settings" view_template="edit_settings.html" to={Routes.user_settings_path(@conn, :edit_settings)} />
+      <.tab conn={@conn} label="Email" view_template="edit_email.html" to={Routes.user_settings_path(@conn, :edit_email)} />
+      <.tab conn={@conn} label="Password" view_template="edit_password.html" to={Routes.user_settings_path(@conn, :edit_password)} />
     </div>
     """
   end
 
-  def settings_section(assigns) do
+  def tab(assigns) do
+    base_class = ~w{px-2 py-1 border-2 border-b-0 border-blue-600 rounded-t-md font-bold}
+
+    class =
+      if view_template(assigns.conn) == assigns.view_template do
+        ~w{text-white bg-blue-600}
+      else
+        ~w{text-blue-600}
+      end
+
     ~H"""
-    <h3 class="text-lg font-bold"><%= @title %></h3>
-    <div class="mt-2 bg-gray-100 border border-gray-600 rounded-lg p-6 pt-4">
-      <%= render_slot(@inner_block) %>
-    </div>
+    <%= link(to: @to, class: base_class ++ class) do %>
+      <%= @label %>
+    <% end %>
     """
   end
 end
