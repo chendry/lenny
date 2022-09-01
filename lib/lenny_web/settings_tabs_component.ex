f56fd83698048a4425dee5c9c81c749be885e8f0
Chad Hendry <chendry@chendry.org>
2022-09-01 10:48:04 -0500

    making tabs more compact so they render on mobile

--- lib/lenny_web/settings_tabs_component.ex
+++ lib/lenny_web/settings_tabs_component.ex
@@ -5,23 +5,23 @@ defmodule LennyWeb.SettingsTabsComponent do
 
   def settings_tabs(assigns) do
     ~H"""
-    <div id="tabs" class="flex flex-row mb-4 font-bold text-blue-600 border-b-2 border-blue-600 space-x-3">
+    <div id="tabs" class="flex flex-row mb-4 font-bold text-blue-600 space-x-2">
       <.tab active={@selected == :settings} label="Settings" to={Routes.user_settings_path(Endpoint, :edit_settings)} />
       <.tab active={@selected == :email} label="Email" to={Routes.user_settings_path(Endpoint, :edit_email)} />
       <.tab active={@selected == :password} label="Password" to={Routes.user_settings_path(Endpoint, :edit_password)} />
-      <.tab active={@selected == :phone} label="Phone Number" to={Routes.phone_number_path(Endpoint, :new)} />
+      <.tab active={@selected == :phone} label="Phone" to={Routes.phone_number_path(Endpoint, :new)} />
     </div>
     """
   end
 
   defp tab(assigns) do
-    base_class = ~w{px-2 py-1 border-2 border-b-0 border-blue-600 rounded-t-md font-bold}
+    base_class = ~w{-mx-1 text-sm px-1 pb-0.5 rounded-t-md font-bold}
 
     class =
       if assigns.active do
-        ~w{text-white bg-blue-600}
+        ~w{border-b-2 border-blue-600}
       else
-        ~w{text-blue-600}
+        ~w{}
       end
 
     ~H"""
