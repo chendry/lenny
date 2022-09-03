834e15e0ccee85daf53b6a4d88aaa3cb7f9ff7aa
Chad Hendry <chendry@chendry.org>
2022-09-03 06:20:39 -0500

    styling flash messages

--- lib/lenny_web/views/layout_view.ex
+++ lib/lenny_web/views/layout_view.ex
@@ -4,4 +4,36 @@ defmodule LennyWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def flash(assigns) do
+    ~H"""
+    <%= if @msg do %>
+      <div class={class(@key)} {click_handler_attrs(@live)}>
+        <span class="float-right ml-2 mb-1">
+          <.x_mark_svg />
+        </span>
+        <%= @msg %>
+      </div>
+    <% end %>
+    """
+  end
+
+  defp click_handler_attrs(live) do
+    if live,
+      do: %{"phx-click" => "lv:clear-flash"},
+      else: %{"onclick" => "javascript:this.remove()"}
+  end
+
+  defp x_mark_svg(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+    </svg>
+    """
+  end
+
+  defp class(:info), do: base_class() ++ ~w{bg-blue-100 border-blue-400 text-blue-800}
+  defp class(:error), do: base_class() ++ ~w{bg-red-200 border-red-600 text-red-600}
+
+  defp base_class, do: ~w{cursor-pointer mt-4 border px-4 py-2 rounded-md}
 end
