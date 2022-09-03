e0b68259d32769e4100dda64e6498f2d4556ea3b
Chad Hendry <chendry@chendry.org>
2022-09-03 06:49:10 -0500

    moving container div to layout

--- lib/lenny_web/live/calls_live.ex
+++ lib/lenny_web/live/calls_live.ex
@@ -47,81 +47,75 @@ defmodule LennyWeb.CallsLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="mt-6 container mx-auto pb-12">
-      <%= if @phone_number == nil do %>
-        <div class="px-6 sm:px-0">
-          <%= live_render @socket, LennyWeb.PhoneNumberLive, id: "phone_number_live" %>
+    <%= if @phone_number == nil do %>
+      <%= live_render @socket, LennyWeb.PhoneNumberLive, id: "phone_number_live" %>
+    <% else %>
+      <div class="bg-slate-100 border border-slate-600 rounded-lg shadow-md p-4 text-center">
+        <h1 class="text-xl font-bold">
+          Your Verified Phone Number
+        </h1>
+        <div class="text-green-600 text-xl font-bold tracking-[0.25rem]">
+          <span id="approved-number"><%= @phone_number.phone %></span>
         </div>
-      <% else %>
-        <div class="px-6 sm:px-0">
-          <div class="bg-slate-100 border border-slate-600 rounded-lg shadow-md p-4 text-center">
-            <h1 class="text-xl font-bold">
-              Your Verified Phone Number
-            </h1>
-            <div class="text-green-600 text-xl font-bold tracking-[0.25rem]">
-              <span id="approved-number"><%= @phone_number.phone %></span>
-            </div>
-          </div>
+      </div>
 
-          <p class="mt-4">
-            This page automatically refreshes when you call
-            <a class="font-bold whitespace-nowrap" href="tel:938GOLENNY">938-GOLENNY</a>
-            from your verified phone number.
-          </p>
+      <p class="mt-4">
+        This page automatically refreshes when you call
+        <a class="font-bold whitespace-nowrap" href="tel:938GOLENNY">938-GOLENNY</a>
+        from your verified phone number.
+      </p>
 
-          <div class="mt-10">
-            <.live_component
-              module={ForwardingInstructionsLive}
-              id="forwarding-instructions"
-              carrier={@phone_number.carrier["name"]}
-            />
-          </div>
-        </div>
+      <div class="mt-10">
+        <.live_component
+          module={ForwardingInstructionsLive}
+          id="forwarding-instructions"
+          carrier={@phone_number.carrier["name"]}
+        />
+      </div>
 
-        <%= if not Enum.empty?(@call_history) do %>
-          <h1 class="mt-10 px-6 sm:px-0 text-lg font-bold">
-            Call History
-          </h1>
+      <%= if not Enum.empty?(@call_history) do %>
+        <h1 class="mt-10 text-lg font-bold">
+          Call History
+        </h1>
 
-          <div class="flex flex-col mt-2 border-y sm:border sm:rounded-lg sm:overflow-hidden border-gray-400">
-            <%= for row <- @call_history do %>
-              <%= live_redirect to: "/calls/#{row.sid}" do %>
-                <%= if row != List.first(@call_history) do %>
-                  <div class="border-t border-gray-400" />
-                <% end %>
-                <div class="bg-gray-100 py-2 px-6" id={"call-#{row.sid}"}>
-                  <div class="flex flex-row justify-between">
-                    <span>
-                      <span class="font-bold"><%= Calls.format_timestamp_date(row.started_at) %></span>
-                      <span class="ml-2"><%= Calls.format_timestamp_time(row.started_at) %></span>
-                    </span>
-
-                    <span class="font-bold">
-                      <%= if row.ended_at == nil do %>
-                        <span class="text-green-700">Connected</span>
-                      <% else %>
-                        <span class="text-gray-600">
-                          <%= Calls.format_duration(row.started_at, row.ended_at) %>
-                        </span>
-                      <% end %>
-                  </span>
-                  </div>
-
-                  <div class="flex flex-row justify-between">
-                    <span class="tracking-widest">
-                      <%= row.from %>
-                    </span>
-                    <%= if row.recorded do %>
-                      <span class="font-bold text-red-600">Recorded</span>
-                    <% end %>
-                  </div>
-                </div>
+        <div class="flex flex-col -mx-6 sm:mx-0 mt-2 border-y sm:border sm:rounded-lg sm:overflow-hidden border-gray-400">
+          <%= for row <- @call_history do %>
+            <%= live_redirect to: "/calls/#{row.sid}" do %>
+              <%= if row != List.first(@call_history) do %>
+                <div class="border-t border-gray-400" />
               <% end %>
+              <div class="bg-gray-100 py-2 px-6" id={"call-#{row.sid}"}>
+                <div class="flex flex-row justify-between">
+                  <span>
+                    <span class="font-bold"><%= Calls.format_timestamp_date(row.started_at) %></span>
+                    <span class="ml-2"><%= Calls.format_timestamp_time(row.started_at) %></span>
+                  </span>
+
+                  <span class="font-bold">
+                    <%= if row.ended_at == nil do %>
+                      <span class="text-green-700">Connected</span>
+                    <% else %>
+                      <span class="text-gray-600">
+                        <%= Calls.format_duration(row.started_at, row.ended_at) %>
+                      </span>
+                    <% end %>
+                </span>
+                </div>
+
+                <div class="flex flex-row justify-between">
+                  <span class="tracking-widest">
+                    <%= row.from %>
+                  </span>
+                  <%= if row.recorded do %>
+                    <span class="font-bold text-red-600">Recorded</span>
+                  <% end %>
+                </div>
+              </div>
             <% end %>
-          </div>
-        <% end %>
+          <% end %>
+        </div>
       <% end %>
-    </div>
+    <% end %>
     """
   end
 
