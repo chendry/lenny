4f70540598927087895b2252fbe587bb194166fc
Chad Hendry <chendry@chendry.org>
2022-09-03 05:30:48 -0500

    moving settings tabs into UserSettingsView

--- lib/lenny_web/views/user_settings_view.ex
+++ lib/lenny_web/views/user_settings_view.ex
@@ -1,7 +1,7 @@
 defmodule LennyWeb.UserSettingsView do
   use LennyWeb, :view
 
-  import LennyWeb.SettingsTabsComponent
+  alias LennyWeb.Endpoint
 
   def breadcrumbs(assigns) do
     ~H"""
@@ -14,16 +14,39 @@ defmodule LennyWeb.UserSettingsView do
   end
 
   def tabs(assigns) do
-    selected =
-      case view_template(assigns.conn) do
-        "edit_settings.html" -> :settings
-        "edit_email.html" -> :email
-        "edit_password.html" -> :password
-        "edit_phone.html" -> :phone
-      end
-    
     ~H"""
-    <.settings_tabs conn={@conn} selected={selected} />
+    <div id="tabs" class="flex flex-row justify-between sm:justify-start sm:space-x-4 mb-4 font-bold text-blue-600 space-x-2">
+      <.tab active={selected_tab(@conn) == :settings} label="Settings" to={Routes.user_settings_path(Endpoint, :edit_settings)} />
+      <.tab active={selected_tab(@conn) == :email} label="Email" to={Routes.user_settings_path(Endpoint, :edit_email)} />
+      <.tab active={selected_tab(@conn) == :password} label="Password" to={Routes.user_settings_path(Endpoint, :edit_password)} />
+      <.tab active={selected_tab(@conn) == :phone} label="Phone" to={Routes.user_settings_path(Endpoint, :edit_phone)} />
+    </div>
+    """
+  end
+
+  def selected_tab(conn) do
+    case view_template(conn) do
+      "edit_settings.html" -> :settings
+      "edit_email.html" -> :email
+      "edit_password.html" -> :password
+      "edit_phone.html" -> :phone
+    end
+  end
+
+  defp tab(assigns) do
+    base_class = ~w{pb-0.5 rounded-t-md font-bold}
+
+    class =
+      if assigns.active do
+        ~w{border-b-2 border-blue-600}
+      else
+        ~w{}
+      end
+
+    ~H"""
+    <%= link(to: @to, class: base_class ++ class) do %>
+      <%= @label %>
+    <% end %>
     """
   end
 end
