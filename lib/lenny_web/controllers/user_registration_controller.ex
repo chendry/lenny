b9379ed4ced62ddee4fc9d92850f4833693eae2a
Chad Hendry <chendry@chendry.org>
2022-09-03 12:21:15 -0500

    not showing flash message during registration

--- lib/lenny_web/controllers/user_registration_controller.ex
+++ lib/lenny_web/controllers/user_registration_controller.ex
@@ -20,7 +20,6 @@ defmodule LennyWeb.UserRegistrationController do
           )
 
         conn
-        |> put_flash(:info, "User created successfully.")
         |> UserAuth.log_in_user(user)
 
       {:error, %Ecto.Changeset{} = changeset} ->
