b5d2e6261f1c139313b284a35ee1bc325d0516b9
Chad Hendry <chendry@chendry.org>
2022-09-05 08:11:04 -0500

    cleaning up CallsLiveTest

--- test/lenny_web/live/calls_live_test.exs
+++ test/lenny_web/live/calls_live_test.exs
@@ -12,82 +12,55 @@ defmodule LennyWeb.CallsLiveTest do
   setup [:register_and_log_in_user]
 
   test "incoming calls trigger redirect to call", %{conn: conn, user: user} do
-    Mox.expect(Lenny.TwilioMock, :send_sms, fn _, _ -> :ok end)
+    phone_number_fixture(user, phone: "555-0000")
 
-    phone_number_fixture(user, phone: "+13126180256")
+    Mox.expect(Lenny.TwilioMock, :send_sms, fn "555-0000", _ -> :ok end)
 
     {:ok, live_view, _html} = live(conn, "/calls")
 
     Phoenix.ConnTest.build_conn()
-    |> post(
-      "/twilio/incoming",
-      %{
-        "AccountSid" => "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-        "ApiVersion" => "2010-04-01",
-        "CallSid" => "CAcd3d0f9f054366f89712ef4278630247",
-        "CallStatus" => "ringing",
-        "CallToken" =>
-          "%7B%22parentCallInfoToken%22%3A%22eyJhbGciOiJFUzI1NiJ9.eyJjYWxsU2lkIjoiQ0FjZDNkMGY5ZjA1NDM2NmY4OTcxMmVmNDI3ODYzMDI0NyIsImZyb20iOiIrMTMxMjYxODAyNTYiLCJ0byI6IisxOTM4NDY1MzY2OSIsImlhdCI6IjE2NjE0NDYzNjIifQ.IA5Hg2PlrYTH38bybo4WQ-vzgg5hSGH3UQZRjNWxTo1hpXDUydPMyJdigR5BmN4f2gllKMR9_Ua6VoKuUOPhlw%22%2C%22identityHeaderTokens%22%3A%5B%5D%7D",
-        "Called" => "+19384653669",
-        "CalledCity" => "",
-        "CalledCountry" => "US",
-        "CalledState" => "AL",
-        "CalledZip" => "",
-        "Caller" => "+13126180256",
-        "CallerCity" => "CHICAGO",
-        "CallerCountry" => "US",
-        "CallerState" => "IL",
-        "CallerZip" => "60605",
-        "Direction" => "inbound",
-        "From" => "+13126180256",
-        "FromCity" => "CHICAGO",
-        "FromCountry" => "US",
-        "FromState" => "IL",
-        "FromZip" => "60605",
-        "To" => "+19384653669",
-        "ToCity" => "",
-        "ToCountry" => "US",
-        "ToState" => "AL",
-        "ToZip" => ""
-      }
-    )
+    |> post("/twilio/incoming", %{
+      "CallSid" => "CA46d9",
+      "From" => "555-0000",
+      "To" => "+19384653669"
+    })
 
-    assert_redirect(live_view, "/calls/CAcd3d0f9f054366f89712ef4278630247")
+    assert_redirect(live_view, "/calls/CA46d9")
   end
 
   test "/calls prompts for phone number if there is no verified or pending phone number", %{
     conn: conn
   } do
     {:ok, _live_view, html} = live(conn, "/calls")
-    assert html =~ "Get started by verifying"
+    assert html =~ ~s(phx-submit="register_phone_number")
   end
 
-  test "/calls prompts for verification if there is a pending phone number and no verified phone number",
+  test "/calls prompts for verification if there is a pending phone number but no verified phone number",
        %{conn: conn, user: user} do
     phone_number_fixture(user, verified_at: nil)
     {:ok, _live_view, html} = live(conn, "/calls")
-    assert html =~ "We sent a code to"
+    assert html =~ ~s(phx-submit="verify_phone_number")
   end
 
   test "delete a call", %{conn: conn, user: user} do
-    phone_number_fixture(user, phone: "+15552223333", verified_at: ~N[2022-08-29 12:14:59])
+    phone_number_fixture(user, phone: "555-0000", verified_at: ~N[2022-08-29 12:14:59])
 
-    c1 = call_fixture(sid: "CA0001", from: "+15552220001", ended_at: ~N[2022-08-29 13:31:38])
-    c2 = call_fixture(sid: "CA0002", from: "+15552220002", ended_at: ~N[2022-08-29 13:31:57])
+    c1 = call_fixture(sid: "CAb0f3", from: "555-0001", ended_at: ~N[2022-08-29 13:31:38])
+    c2 = call_fixture(sid: "CA6a4b", from: "555-0002", ended_at: ~N[2022-08-29 13:31:57])
 
     users_calls_fixture(user, c1)
     users_calls_fixture(user, c2)
 
     {:ok, live_view, html} = live(conn, "/calls")
 
-    assert html =~ "+15552220001"
-    assert html =~ "+15552220002"
+    assert html =~ "555-0001"
+    assert html =~ "555-0001"
 
     {:ok, live_view, _html} =
       live_view
-      |> element("a", "+15552220001")
+      |> element("a", "555-0001")
       |> render_click()
-      |> follow_redirect(conn, "/calls/CA0001")
+      |> follow_redirect(conn, "/calls/CAb0f3")
 
     _html =
       live_view
@@ -100,44 +73,40 @@ defmodule LennyWeb.CallsLiveTest do
       |> render_click()
       |> follow_redirect(conn, "/calls")
 
-    refute html =~ "+15552220001"
-    assert html =~ "+15552220002"
+    refute html =~ "555-0001"
+    assert html =~ "555-0002"
   end
 
-  test "loading the wait page redirects to the single active call", %{conn: conn, user: user} do
-    phone_number_fixture(user, phone: "+15555551234", verified_at: ~N[2022-08-29 15:00:41])
-
-    c = call_fixture(sid: "CA0001", ended_at: nil)
+  test "loading /calls redirects to the single active call", %{conn: conn, user: user} do
+    c = call_fixture(sid: "CA34c5", ended_at: nil)
     uc = users_calls_fixture(user, c, seen_at: nil)
 
     {:ok, _live_view, _html} =
       live(conn, "/calls")
-      |> follow_redirect(conn, "/calls/CA0001")
+      |> follow_redirect(conn, "/calls/CA34c5")
 
     assert Repo.reload!(uc).seen_at != nil
 
-    {:ok, _live_view, html} = live(conn, "/calls")
-
-    assert html =~ "Your Verified Phone Number"
+    {:ok, _live_view, _html} = live(conn, "/calls")
   end
 
-  test "/calls automatically updates active calls", %{conn: conn, user: user} do
-    phone_number_fixture(user, phone: "+15551231234")
+  test "/calls automatically updates active calls when their status changes", %{conn: conn, user: user} do
+    phone_number_fixture(user)
 
-    c1 = call_fixture(from: "+12223330001", ended_at: ~N[2022-08-29 15:12:24])
-    c2 = call_fixture(from: "+12223330002", ended_at: nil)
+    c1 = call_fixture(sid: "CA429c", ended_at: ~N[2022-08-29 15:12:24])
+    c2 = call_fixture(sid: "CAc8e6", ended_at: nil)
 
     users_calls_fixture(user, c1, seen_at: ~N[2022-08-29 15:14:00])
     users_calls_fixture(user, c2, seen_at: ~N[2022-08-29 15:14:30])
 
     {:ok, live_view, _html} = live(conn, "/calls")
-    refute live_view |> element("#call-#{c1.sid}") |> render() =~ "Connected"
-    assert live_view |> element("#call-#{c2.sid}") |> render() =~ "Connected"
+    refute live_view |> element("#call-CA429c") |> render() =~ "Connected"
+    assert live_view |> element("#call-CAc8e6") |> render() =~ "Connected"
 
     Phoenix.ConnTest.build_conn()
     |> post("/twilio/status/call", %{"CallSid" => c2.sid, "CallStatus" => "completed"})
 
-    refute live_view |> element("#call-#{c1.sid}") |> render() =~ "Connected"
-    refute live_view |> element("#call-#{c2.sid}") |> render() =~ "Connected"
+    refute live_view |> element("#call-CA429c") |> render() =~ "Connected"
+    refute live_view |> element("#call-CAc8e6") |> render() =~ "Connected"
   end
 end
