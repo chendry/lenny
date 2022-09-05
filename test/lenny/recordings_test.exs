a82f7566a07b3c3b42db83fd1e6076670a04026b
Chad Hendry <chendry@chendry.org>
2022-09-05 07:00:06 -0500

    cleaning up RecordingsTest

--- test/lenny/recordings_test.exs
+++ test/lenny/recordings_test.exs
@@ -12,16 +12,9 @@ defmodule Lenny.RecordingsTest do
 
   test "create a recording using twilio params" do
     params = %{
-      "AccountSid" => "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      "CallSid" => "CAcfcd3db14f143b00141b45cd0ffa3d65",
-      "RecordingChannels" => "1",
-      "RecordingSid" => "REafd66b74b49551a69593c41da1d638c0",
-      "RecordingSource" => "StartCallRecordingAPI",
-      "RecordingStartTime" => "Sat, 27 Aug 2022 11:03:41 +0000",
+      "CallSid" => "CAb965",
       "RecordingStatus" => "in-progress",
-      "RecordingTrack" => "both",
-      "RecordingUrl" =>
-        "https://api.twilio.com/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Recordings/REafd66b74b49551a69593c41da1d638c0"
+      "RecordingUrl" => "https://example.com/CAb965.wav"
     }
 
     recording =
@@ -29,61 +22,45 @@ defmodule Lenny.RecordingsTest do
       |> Recordings.insert_or_update_from_twilio_params!()
       |> Repo.reload()
 
-    assert recording.sid == "CAcfcd3db14f143b00141b45cd0ffa3d65"
+    assert recording.sid == "CAb965"
     assert recording.status == "in-progress"
-
-    assert recording.url ==
-             "https://api.twilio.com/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Recordings/REafd66b74b49551a69593c41da1d638c0"
-
+    assert recording.url == "https://example.com/CAb965.wav"
     assert recording.params == params
   end
 
   test "update a recording using twilio params" do
     recording =
       recordings_fixture(
-        sid: "CAcfcd3db14f143b00141b45cd0ffa3d65",
+        sid: "CA73ee",
         status: "in-progress",
-        url: "https://foo.bar/",
-        params: %{}
+        url: "https://example.com/CA73ee.wav"
       )
 
     Recordings.insert_or_update_from_twilio_params!(%{
-      "AccountSid" => "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      "CallSid" => "CAcfcd3db14f143b00141b45cd0ffa3d65",
-      "ErrorCode" => "0",
-      "RecordingChannels" => "1",
-      "RecordingDuration" => "10",
-      "RecordingSid" => "REafd66b74b49551a69593c41da1d638c0",
-      "RecordingSource" => "StartCallRecordingAPI",
-      "RecordingStartTime" => "Sat, 27 Aug 2022 11:03:41 +0000",
+      "CallSid" => "CA73ee",
       "RecordingStatus" => "completed",
-      "RecordingTrack" => "both",
-      "RecordingUrl" =>
-        "https://api.twilio.com/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Recordings/REafd66b74b49551a69593c41da1d638c0"
+      "RecordingUrl" => "https://example.com/CA73ee.mp3"
     })
 
     recording = Repo.reload(recording)
-
     assert recording.status == "completed"
-
-    assert recording.url ==
-             "https://api.twilio.com/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Recordings/REafd66b74b49551a69593c41da1d638c0"
+    assert recording.url == "https://example.com/CA73ee.mp3"
   end
 
   test "get_recording_for_user" do
     u1 = user_fixture()
     u2 = user_fixture()
 
-    phone_number_fixture(u1, phone: "+15554443333")
-    phone_number_fixture(u2, phone: "+15554443333")
+    phone_number_fixture(u1)
+    phone_number_fixture(u2)
 
-    c = call_fixture(sid: "CA001")
-    r = recordings_fixture(sid: "CA001")
+    c = call_fixture(sid: "CAb103")
+    r = recordings_fixture(sid: "CAb103")
 
     users_calls_fixture(u1, c, recorded: true)
     users_calls_fixture(u2, c, recorded: false)
 
-    assert Recordings.get_recording_for_user(u1.id, "CA001") == r
-    assert Recordings.get_recording_for_user(u2.id, "CA001") == nil
+    assert Recordings.get_recording_for_user(u1.id, "CAb103") == r
+    assert Recordings.get_recording_for_user(u2.id, "CAb103") == nil
   end
 end
