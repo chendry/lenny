7e518897fb514198d2d3a8ae3551c3bfd96112ef
Chad Hendry <chendry@chendry.org>
2022-09-05 07:21:30 -0500

    cleaning up CallLiveTest

--- test/lenny_web/live/call_live_test.exs
+++ test/lenny_web/live/call_live_test.exs
@@ -8,41 +8,33 @@ defmodule LennyWeb.CallLiveTest do
   import Lenny.UsersCallsFixtures
 
   alias Lenny.Repo
-  alias Lenny.Calls.Call
 
   describe "when authenticated" do
     setup [:register_and_log_in_user]
 
     test "call status webhook with CallStatus=completed ends the call", %{conn: conn, user: user} do
-      call = call_fixture(sid: "CAXXX")
+      call = call_fixture(sid: "CAf173")
       users_calls_fixture(user, call)
 
-      {:ok, live_view, html} = live(conn, "/calls/CAXXX")
-      assert html =~ ~S(data-sid="CAXXX")
+      {:ok, live_view, html} = live(conn, "/calls/CAf173")
+      assert html =~ ~S(data-sid="CAf173")
 
       Phoenix.ConnTest.build_conn()
-      |> post("/twilio/status/call", %{"CallSid" => "CAXXX", "CallStatus" => "completed"})
+      |> post("/twilio/status/call", %{"CallSid" => "CAf173", "CallStatus" => "completed"})
 
       assert render(live_view) =~ "Call ended"
-
-      assert Repo.get(Call, call.id).ended_at != nil
+      assert Repo.reload!(call).ended_at != nil
     end
 
     test "push the say buttons during a call", %{conn: conn, user: user} do
-      phone_number_fixture(user, phone: "+13126180256")
-      call = call_fixture(sid: "CAXXXX1234", from: "+13126180256")
+      call = call_fixture(sid: "CAa820")
       users_calls_fixture(user, call)
 
-      {:ok, live_view, _html} = live(conn, "/calls/CAXXXX1234")
+      {:ok, live_view, _html} = live(conn, "/calls/CAa820")
 
-      Lenny.TwilioMock
-      |> Mox.expect(:modify_call, fn "CAXXXX1234", twiml ->
+      Mox.expect(Lenny.TwilioMock, :modify_call, fn "CAa820", twiml ->
         assert twiml =~ "lenny_01.mp3"
         assert twiml =~ "/twilio/gather/1"
-      end)
-      |> Mox.expect(:modify_call, fn "CAXXXX1234", twiml ->
-        assert twiml =~ "lenny_03.mp3"
-        assert twiml =~ "/twilio/gather/3"
       end)
 
       _html =
@@ -50,42 +42,42 @@ defmodule LennyWeb.CallLiveTest do
         |> element("button#say_01")
         |> render_click()
 
+      Mox.expect(Lenny.TwilioMock, :modify_call, fn "CAa820", twiml ->
+        assert twiml =~ "lenny_03.mp3"
+        assert twiml =~ "/twilio/gather/3"
+      end)
+
       _html =
         live_view
         |> element("button#say_03", "")
         |> render_click()
     end
 
-    test "push the say buttons with autopilot off and on", %{conn: conn, user: user} do
-      phone_number_fixture(user, phone: "+13126180256")
-      call = call_fixture(sid: "CAXXXX1234", from: "+13126180256")
+    test "clicking the autopilot checkbox toggles autopilot on the record", %{conn: conn, user: user} do
+      call = call_fixture(sid: "CA7f8b", autopilot: true)
       users_calls_fixture(user, call)
 
-      {:ok, live_view, _html} = live(conn, "/calls/CAXXXX1234")
+      {:ok, live_view, _html} = live(conn, "/calls/CA7f8b")
 
-      Lenny.TwilioMock
-      |> Mox.expect(:modify_call, fn "CAXXXX1234", twiml ->
-        assert twiml =~ "lenny_07.mp3"
-        assert twiml =~ "/twilio/gather/7"
-      end)
-      |> Mox.expect(:modify_call, fn "CAXXXX1234", twiml ->
-        assert twiml =~ "lenny_07.mp3"
-        refute twiml =~ "/twilio/gather/7"
-      end)
+      live_view
+      |> element("#autopilot")
+      |> render_click()
+
+      assert Repo.reload(call).autopilot == false
+    end
+
+    test "push the say buttons with autopilot on", %{conn: conn, user: user} do
+      call = call_fixture(sid: "CA4b93", autopilot: true)
+      users_calls_fixture(user, call)
+
+      {:ok, live_view, _html} = live(conn, "/calls/CA4b93")
 
       assert render(element(live_view, "#autopilot")) =~ "checked"
 
-      _html =
-        live_view
-        |> element("button#say_07", "")
-        |> render_click()
-
-      _html =
-        live_view
-        |> element("#autopilot")
-        |> render_click()
-
-      refute render(element(live_view, "#autopilot")) =~ "checked"
+      Mox.expect(Lenny.TwilioMock, :modify_call, fn "CA4b93", twiml ->
+        assert twiml =~ "lenny_07.mp3"
+        assert twiml =~ "/twilio/gather/7"
+      end)
 
       _html =
         live_view
@@ -93,25 +85,43 @@ defmodule LennyWeb.CallLiveTest do
         |> render_click()
     end
 
-    test "push the DTMF buttons", %{conn: conn, user: user} do
-      phone_number_fixture(user, phone: "+13126180256")
-      call = call_fixture(sid: "CAXXXX1234", from: "+13126180256")
+    test "push the say buttons with autopilot off", %{conn: conn, user: user} do
+      call = call_fixture(sid: "CA02fc", autopilot: false)
       users_calls_fixture(user, call)
 
-      {:ok, live_view, _html} = live(conn, "/calls/CAXXXX1234")
+      {:ok, live_view, _html} = live(conn, "/calls/CA02fc")
 
-      Lenny.TwilioMock
-      |> Mox.expect(:modify_call, fn "CAXXXX1234", twiml ->
-        assert twiml =~ ~r{<Response>\s*<Play digits="1"}
+      refute render(element(live_view, "#autopilot")) =~ "checked"
+
+      Mox.expect(Lenny.TwilioMock, :modify_call, fn "CA02fc", twiml ->
+        assert twiml =~ "lenny_07.mp3"
+        refute twiml =~ "/twilio/gather/7"
       end)
-      |> Mox.expect(:modify_call, fn "CAXXXX1234", twiml ->
-        assert twiml =~ ~r{<Response>\s*<Play digits="#"}
+
+      live_view
+      |> element("button#say_07", "")
+      |> render_click()
+    end
+
+    test "push the DTMF buttons", %{conn: conn, user: user} do
+      phone_number_fixture(user)
+      call = call_fixture(sid: "CA7923")
+      users_calls_fixture(user, call)
+
+      {:ok, live_view, _html} = live(conn, "/calls/CA7923")
+
+      Mox.expect(Lenny.TwilioMock, :modify_call, fn "CA7923", twiml ->
+        assert twiml =~ ~r{<Response>\s*<Play digits="1"}
       end)
 
       _html =
         live_view
         |> element("#dtmf-1")
         |> render_click()
+
+      Mox.expect(Lenny.TwilioMock, :modify_call, fn "CA7923", twiml ->
+        assert twiml =~ ~r{<Response>\s*<Play digits="#"}
+      end)
 
       _html =
         live_view
@@ -120,97 +130,64 @@ defmodule LennyWeb.CallLiveTest do
     end
 
     test "push the silence button", %{conn: conn, user: user} do
-      phone_number_fixture(user, phone: "+13126180256")
-      call = call_fixture(sid: "CAXXXX1234", from: "+13126180256")
+      call = call_fixture(sid: "CA6b37")
       users_calls_fixture(user, call)
 
-      {:ok, live_view, _html} = live(conn, "/calls/CAXXXX1234")
+      {:ok, live_view, _html} = live(conn, "/calls/CA6b37")
 
-      Lenny.TwilioMock
-      |> Mox.expect(:modify_call, fn "CAXXXX1234", twiml ->
+      Mox.expect(Lenny.TwilioMock, :modify_call, fn "CA6b37", twiml ->
         assert twiml =~ ~s{<Pause length="120" />}
         refute twiml =~ ".mp3"
       end)
 
-      _html =
-        live_view
-        |> element("#silence")
-        |> render_click()
+      live_view
+      |> element("#silence")
+      |> render_click()
     end
 
     test "hang up", %{conn: conn, user: user} do
-      phone_number_fixture(user, phone: "+13126180256")
-      call = call_fixture(sid: "CAXXXX1234", from: "+13126180256")
+      call = call_fixture(sid: "CA4dc0")
       users_calls_fixture(user, call)
 
-      {:ok, live_view, _html} = live(conn, "/calls/CAXXXX1234")
+      {:ok, live_view, _html} = live(conn, "/calls/CA4dc0")
 
-      Lenny.TwilioMock
-      |> Mox.expect(:modify_call, fn "CAXXXX1234", twiml ->
+      Mox.expect(Lenny.TwilioMock, :modify_call, fn "CA4dc0", twiml ->
         assert twiml =~ "<Hangup />"
       end)
 
-      assert Repo.get(Call, call.id).ended_at == nil
+      assert Repo.reload!(call).ended_at == nil
 
       _html =
         live_view
         |> element("#hangup")
         |> render_click()
 
-      assert Repo.get(Call, call.id).ended_at != nil
+      assert Repo.reload!(call).ended_at != nil
     end
 
     test "visit a call that has ended", %{conn: conn, user: user} do
-      call = call_fixture(sid: "CA007", ended_at: ~N[2022-08-26 18:07:43])
+      call = call_fixture(sid: "CAfc43", ended_at: ~N[2022-08-26 18:07:43])
       users_calls_fixture(user, call)
 
-      {:ok, _live_view, html} = live(conn, "/calls/CA007")
+      {:ok, _live_view, html} = live(conn, "/calls/CAfc43")
+
       assert html =~ "Call ended"
       refute html =~ "Start Audio"
     end
 
-    test "show what the person says during autopilot", %{conn: conn, user: user} do
-      call = call_fixture(sid: "CA165c28bffa7817b0ccd857fa1adc124c")
+    test "show what the scammer says", %{conn: conn, user: user} do
+      call = call_fixture(sid: "CA7b20")
       users_calls_fixture(user, call)
 
-      {:ok, live_view, html} = live(conn, "/calls/CA165c28bffa7817b0ccd857fa1adc124c")
+      {:ok, live_view, html} = live(conn, "/calls/CA7b20")
 
       refute html =~ "I'm a banana"
 
       Phoenix.ConnTest.build_conn()
-      |> post(
-        "/twilio/gather/3",
-        %{
-          "AccountSid" => "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-          "ApiVersion" => "2010-04-01",
-          "CallSid" => "CA165c28bffa7817b0ccd857fa1adc124c",
-          "CallStatus" => "in-progress",
-          "Called" => "+19384653669",
-          "CalledCity" => "",
-          "CalledCountry" => "US",
-          "CalledState" => "AL",
-          "CalledZip" => "",
-          "Caller" => "+13126180256",
-          "CallerCity" => "CHICAGO",
-          "CallerCountry" => "US",
-          "CallerState" => "IL",
-          "CallerZip" => "60605",
-          "Confidence" => "0.91283864",
-          "Direction" => "inbound",
-          "From" => "+13126180256",
-          "FromCity" => "CHICAGO",
-          "FromCountry" => "US",
-          "FromState" => "IL",
-          "FromZip" => "60605",
-          "Language" => "en-US",
-          "SpeechResult" => "I'm a banana.",
-          "To" => "+19384653669",
-          "ToCity" => "",
-          "ToCountry" => "US",
-          "ToState" => "AL",
-          "ToZip" => ""
-        }
-      )
+      |> post("/twilio/gather/3", %{
+        "CallSid" => "CA7b20",
+        "SpeechResult" => "I'm a banana."
+      })
 
       html =
         live_view
@@ -220,62 +197,11 @@ defmodule LennyWeb.CallLiveTest do
       assert html =~ "I&#39;m a banana."
     end
 
-    test "show what the person says without autopilot", %{conn: conn, user: user} do
-      call = call_fixture(sid: "CA165c28bffa7817b0ccd857fa1adc124c", autopilot: true)
-      users_calls_fixture(user, call)
-
-      {:ok, live_view, html} = live(conn, "/calls/CA165c28bffa7817b0ccd857fa1adc124c")
-
-      refute html =~ "We only have yardsticks"
-
-      Phoenix.ConnTest.build_conn()
-      |> post(
-        "/twilio/gather/1",
-        %{
-          "AccountSid" => "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-          "ApiVersion" => "2010-04-01",
-          "CallSid" => "CA165c28bffa7817b0ccd857fa1adc124c",
-          "CallStatus" => "in-progress",
-          "Called" => "+19384653669",
-          "CalledCity" => "",
-          "CalledCountry" => "US",
-          "CalledState" => "AL",
-          "CalledZip" => "",
-          "Caller" => "+13126180256",
-          "CallerCity" => "CHICAGO",
-          "CallerCountry" => "US",
-          "CallerState" => "IL",
-          "CallerZip" => "60605",
-          "Confidence" => "0.91283864",
-          "Direction" => "inbound",
-          "From" => "+13126180256",
-          "FromCity" => "CHICAGO",
-          "FromCountry" => "US",
-          "FromState" => "IL",
-          "FromZip" => "60605",
-          "Language" => "en-US",
-          "SpeechResult" => "We only have yardsticks.",
-          "To" => "+19384653669",
-          "ToCity" => "",
-          "ToCountry" => "US",
-          "ToState" => "AL",
-          "ToZip" => ""
-        }
-      )
-
-      html =
-        live_view
-        |> element("#speech")
-        |> render()
-
-      assert html =~ "We only have yardsticks."
-    end
-
     test "the active button changes in response to say buttons", %{conn: conn, user: user} do
-      call = call_fixture(sid: "CA8aa913b958d95117e0571810014050ec")
+      call = call_fixture(sid: "CA9c8d")
       users_calls_fixture(user, call)
 
-      {:ok, live_view, _html} = live(conn, "/calls/CA8aa913b958d95117e0571810014050ec")
+      {:ok, live_view, _html} = live(conn, "/calls/CA9c8d")
 
       html =
         live_view
@@ -284,8 +210,7 @@ defmodule LennyWeb.CallLiveTest do
 
       assert html =~ "Hello, this is Lenny"
 
-      Lenny.TwilioMock
-      |> Mox.expect(:modify_call, fn _, _ -> nil end)
+      Mox.expect(Lenny.TwilioMock, :modify_call, fn _, _ -> nil end)
 
       live_view
       |> element("button#say_03")
@@ -299,11 +224,11 @@ defmodule LennyWeb.CallLiveTest do
       assert html =~ "Oh good! Yes yes yes yes"
     end
 
-    test "the active button changes in response to autopilot", %{conn: conn, user: user} do
-      call = call_fixture(sid: "CA8aa913b958d95117e0571810014050ec", autopilot: true)
+    test "the active button changes on autopilot when scammer responds", %{conn: conn, user: user} do
+      call = call_fixture(sid: "CA0ad9", autopilot: true)
       users_calls_fixture(user, call)
 
-      {:ok, live_view, _html} = live(conn, "/calls/CA8aa913b958d95117e0571810014050ec")
+      {:ok, live_view, _html} = live(conn, "/calls/CA0ad9")
 
       html =
         live_view
@@ -313,39 +238,11 @@ defmodule LennyWeb.CallLiveTest do
       assert html =~ "Hello, this is Lenny"
 
       Phoenix.ConnTest.build_conn()
-      |> post(
-        "/twilio/gather/0",
-        %{
-          "AccountSid" => "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-          "ApiVersion" => "2010-04-01",
-          "CallSid" => "CA8aa913b958d95117e0571810014050ec",
-          "CallStatus" => "in-progress",
-          "Called" => "+19384653669",
-          "CalledCity" => "",
-          "CalledCountry" => "US",
-          "CalledState" => "AL",
-          "CalledZip" => "",
-          "Caller" => "+13126180256",
-          "CallerCity" => "CHICAGO",
-          "CallerCountry" => "US",
-          "CallerState" => "IL",
-          "CallerZip" => "60605",
-          "Confidence" => "0.8307753",
-          "Direction" => "inbound",
-          "From" => "+13126180256",
-          "FromCity" => "CHICAGO",
-          "FromCountry" => "US",
-          "FromState" => "IL",
-          "FromZip" => "60605",
-          "Language" => "en-US",
-          "SpeechResult" => "Hi Lenny.",
-          "To" => "+19384653669",
-          "ToCity" => "",
-          "ToCountry" => "US",
-          "ToState" => "AL",
-          "ToZip" => ""
-        }
-      )
+      |> post("/twilio/gather/0", %{
+        "CallSid" => "CA0ad9",
+        "CallStatus" => "in-progress",
+        "SpeechResult" => "Hello Lenny"
+      })
 
       html =
         live_view
@@ -355,16 +252,14 @@ defmodule LennyWeb.CallLiveTest do
       assert html =~ "Sorry, I can barely hear &#39;ya there."
     end
 
-    test "autopilot causes the first sound to play after the last sound", %{
+    test "scammer responding to gather on the last sound loops lenny back to the first sound", %{
       conn: conn,
       user: user
     } do
-      call =
-        call_fixture(sid: "CA8aa913b958d95117e0571810014050ec", autopilot: true, iteration: 18)
-
+      call = call_fixture(sid: "CA4c2f", autopilot: true, iteration: 18)
       users_calls_fixture(user, call)
 
-      {:ok, live_view, _html} = live(conn, "/calls/CA8aa913b958d95117e0571810014050ec")
+      {:ok, live_view, _html} = live(conn, "/calls/CA4c2f")
 
       html =
         live_view
@@ -374,39 +269,10 @@ defmodule LennyWeb.CallLiveTest do
       assert html =~ "Sorry, bit of a problem..."
 
       Phoenix.ConnTest.build_conn()
-      |> post(
-        "/twilio/gather/18",
-        %{
-          "AccountSid" => "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-          "ApiVersion" => "2010-04-01",
-          "CallSid" => "CA8aa913b958d95117e0571810014050ec",
-          "CallStatus" => "in-progress",
-          "Called" => "+19384653669",
-          "CalledCity" => "",
-          "CalledCountry" => "US",
-          "CalledState" => "AL",
-          "CalledZip" => "",
-          "Caller" => "+13126180256",
-          "CallerCity" => "CHICAGO",
-          "CallerCountry" => "US",
-          "CallerState" => "IL",
-          "CallerZip" => "60605",
-          "Confidence" => "0.8307753",
-          "Direction" => "inbound",
-          "From" => "+13126180256",
-          "FromCity" => "CHICAGO",
-          "FromCountry" => "US",
-          "FromState" => "IL",
-          "FromZip" => "60605",
-          "Language" => "en-US",
-          "SpeechResult" => "Hi Lenny.",
-          "To" => "+19384653669",
-          "ToCity" => "",
-          "ToCountry" => "US",
-          "ToState" => "AL",
-          "ToZip" => ""
-        }
-      )
+      |> post("/twilio/gather/18", %{
+        "CallSid" => "CA4c2f",
+        "SpeechResult" => "What?"
+      })
 
       html =
         live_view
@@ -417,49 +283,50 @@ defmodule LennyWeb.CallLiveTest do
     end
 
     test "viewing a call marks it as seen for that user", %{user: u1, conn: conn} do
-      call = call_fixture(sid: "CA0001")
+      call = call_fixture(sid: "CAccdf")
 
       u2 = user_fixture()
 
       uc1 = users_calls_fixture(u1, call, seen_at: nil)
       uc2 = users_calls_fixture(u2, call, seen_at: nil)
 
-      {:ok, _live_view, _html} = live(conn, "/calls/CA0001")
+      {:ok, _live_view, _html} = live(conn, "/calls/CAccdf")
 
       assert Repo.reload!(uc1).seen_at != nil
       assert Repo.reload!(uc2).seen_at == nil
     end
 
     test "attempt to view a call that the user doesn't have access to", %{conn: conn} do
-      call = call_fixture(sid: "CA0001")
+      call = call_fixture(sid: "CAb212")
       other_user = user_fixture()
       users_calls_fixture(other_user, call)
 
-      {:error, {:live_redirect, %{flash: %{"alert" => alert}}}} = live(conn, "/calls/CA0001")
+      {:error, {:live_redirect, %{flash: %{"alert" => alert}}}} =
+        live(conn, "/calls/CAb212")
 
       assert alert =~ "You must be logged in"
     end
 
     test "breadcrumbs are visible", %{conn: conn, user: user} do
-      call = call_fixture(sid: "CA001")
+      call = call_fixture(sid: "CA8505")
       users_calls_fixture(user, call)
-      {:ok, live_view, _html} = live(conn, "/calls/CA001")
+      {:ok, live_view, _html} = live(conn, "/calls/CA8505")
       assert live_view |> element("#breadcrumbs") |> has_element?()
     end
   end
 
   describe "when anonymous" do
     test "breadcrumbs are not visible", %{conn: conn} do
-      call_fixture(sid: "CA001")
-      {:ok, live_view, _html} = live(conn, "/calls/CA001")
+      call_fixture(sid: "CA1657")
+      {:ok, live_view, _html} = live(conn, "/calls/CA1657")
       refute live_view |> element("#breadcrumbs") |> has_element?()
     end
 
     test "can't access a finished call", %{conn: conn} do
-      call_fixture(sid: "CA001", ended_at: ~N[2022-08-29 20:11:17])
+      call_fixture(sid: "CA05f5", ended_at: ~N[2022-08-29 20:11:17])
 
       {:error, {:live_redirect, %{flash: %{"alert" => alert}}}} =
-        live(conn, "/calls/CA001")
+        live(conn, "/calls/CA05f5")
 
       assert alert =~ "You must be logged in"
     end
@@ -467,13 +334,16 @@ defmodule LennyWeb.CallLiveTest do
     test "can't access call when any associated user has skip_auth_for_active_calls disabled", %{
       conn: conn
     } do
-      call = call_fixture(sid: "CA001")
+      call = call_fixture(sid: "CAa046")
+
       u1 = user_fixture(skip_auth_for_active_calls: false)
       u2 = user_fixture(skip_auth_for_active_calls: true)
+
       users_calls_fixture(u1, call)
       users_calls_fixture(u2, call)
 
-      {:error, {:live_redirect, %{flash: %{"alert" => alert}}}} = live(conn, "/calls/CA001")
+      {:error, {:live_redirect, %{flash: %{"alert" => alert}}}} =
+        live(conn, "/calls/CAa046")
 
       assert alert =~ "You must be logged in"
     end
